51b3e226ced99b4f757387428557012fe23d963c
stidsem4 <colin@cgoutte.fr>
2021-01-19 08:53:35 +0000

    Différentes methodes pour additionner

--- main.py
+++ main.py
@@ -47,3 +47,29 @@ p2 = Point2D(1, 0)
 print(p1)
 print(p2)
 
+
+def ajout_point2d(p_1, p_2):
+    x = p_1[0] + p_2[0]
+    y = p_1[1] + p_2[1]
+    return Point2D(x, y)
+p3 = ajout_point2d(p1, p2)
+
+print(p3)
+
+print(f"Addition ok: {ajout_point2d(p1, p2) == Point2D(1,1)} ")
+
+def ajout_point2d(p_1, p_2):
+    coord = []
+    for indice in range((len(p_1))):
+        coord.append(p_1[indice] + p_2[indice])
+    return Point2D(coord[0], coord[1])
+
+print(f"Addition ok: {ajout_point2d(p1, p2) == Point2D(1,1)} ")
+
+
+def ajout_point2d(p_1, p_2):
+    coord = tuple(x + y for x, y in zip(p_1, p2))
+    return Point2D(coord[0], coord[1])
+
+print(f"Addition ok: {ajout_point2d(p1, p2) == Point2D(1,1)} ")
+
